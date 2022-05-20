eeb7aea38594f508bf7562efe349a26b64a20646
BORHAN <borhanuddin6888@gmail.com>
2022-05-20 14:24:13 +0600

    React Hooks My Country Project

---
+++ src/components/Country.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './country.css'
+
+const Country = (props) => {
+    const {country} = props;
+    const {name, flags, capital, population, area} = country;
+
+    const handleRemoveCountry = (name) => {
+        props.onRemoveCountry(name);
+    }
+
+  return (
+    <article className='country'>
+        <div>
+            <img src={flags.png} alt={name.common} className='flag' />
+            <h3>Name: {name.common}</h3>
+            <h3>Population: {population}</h3>
+            <h3>Capital: {capital}</h3>
+            <h3>Area: {area}</h3>
+            <button className='btn' onClick={() => {
+                handleRemoveCountry(name.common)
+            }}>
+                Remove Country
+            </button>
+        </div>
+    </article>
+  )
+}
+
+export default Country
